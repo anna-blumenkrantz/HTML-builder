2547a997f4f4fc73be607e11eb4950327a02d239
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-05-06 17:17:35 +0300

    fix: fix to async, exclude directories

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,29 @@ const path = require('path');
 const fs = require('fs');
 
 const filePath = path.join(__dirname, 'secret-folder');
-const files = fs.readdirSync(filePath);
 
-files.forEach(file => {
-    const fileStats = fs.statSync(path.join(filePath, file));
-    const fileSizeInBytes = fileStats.size;
-    const fileSizeInKb = (fileSizeInBytes / 1024);
-    const { name, ext } = path.parse(file);
-    console.log(`${name}-${ext.slice(1)}-${fileSizeInKb}kb`);
+//fetch only file without dircetories
+fs.readdir(filePath, { withFileTypes: true }, (error, files) => {
+  if (error) {
+      console.log(error);
+    }
+   else {
+     var filesOnly = files.filter(files => files.isFile())
+         .map(files => files.name);
+   }
+
+  // Getting information for a file
+  filesOnly.forEach(file => {
+    fs.stat(path.join(filePath, file), (error, fileStats) => {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      const fileSizeInBytes = fileStats.size;
+      const fileSizeInKb = (fileSizeInBytes / 1024).toFixed(3);
+      const { name, ext } = path.parse(file);
+      console.log(`${name}-${ext.slice(1)}-${fileSizeInKb}kb`);
+    }
+    });
   });
-
+});
