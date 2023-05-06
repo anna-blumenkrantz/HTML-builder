6e21f63bf17cc807248c066ec070652687020e1c
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-05-06 18:48:13 +0300

    fix: 05 fix to async function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,25 +4,29 @@ const fs = require('fs');
 //collect source files
 const destDir = path.join(__dirname, 'project-dist');
 const destFile = path.join(destDir, 'bundle.css');
-
+// create a writable stream and create a file
+const destStream = fs.createWriteStream(destFile);
 const stylesDir = path.join(__dirname, 'styles');
-const stylesFiles = fs.readdirSync(stylesDir);
 
-const cssFiles = stylesFiles.filter(file => path.extname(file) === '.css');
-console.log('cssFiles**'+cssFiles);
-
-//merge files to a bundle
-cssFiles.forEach(file => {
-    const srcFile = path.join(stylesDir, file);
-    const readStream = fs.createReadStream(srcFile);
-    console.log('readStream '+readStream);
-
-    readStream.on('data', data => {
-    fs.appendFileSync(destFile,'\n/* Start of ' + file + ' */\n');
-    fs.appendFileSync(destFile, data);
-    });
-    readStream.on('end', () => {
-    fs.appendFileSync(destFile,'\n/* End of ' + file + ' */\n');
-    fs.appendFileSync(destFile, '\n'); // Append line break
-    })
+// collect source files
+fs.readdir(stylesDir, (err, stylesFiles) => {
+  if (err) {
+    throw err;
+  } else {
+        const cssFiles = stylesFiles.filter(file => path.extname(file) === '.css');
+        console.log('cssFiles**'+cssFiles);
+        // merge files to a bundle
+        cssFiles.forEach(file => {
+            const srcFile = path.join(stylesDir, file);
+            const readStream = fs.createReadStream(srcFile);
+            readStream.on('data', data => {
+                destStream.write(`/* Start of ${file} */\n`);
+                destStream.write(data);
+                destStream.write(`\n/* End of ${file} */\n\n`);
+            });
+            readStream.on('end', () => {
+                console.log(`${file} added!`);
+            });
+        });
+    }
 });
