7831c89617c020ec45bf9d3db43be949df09b066
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-05-08 18:38:03 +0300

    fix: add copyDir() according to TZ

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,32 +1,37 @@
 /* eslint-disable no-undef */
 const path = require('path');
 const fs = require('fs');
-//collect source files
-const srcDir = path.join(__dirname, 'files');
 
-//initiate copy files
-const destDir = path.join(__dirname, 'files-copy');
-try {
-  if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir);
-  }
-} catch (err) {
-  console.error(err);
-}
+copyDir();
 
-fs.readdir(srcDir, (error, sourceFiles) => {
-  if (error) {
-      console.log(error);
+function copyDir() {
+  //collect source files
+  const srcDir = path.join(__dirname, 'files');
+
+  //initiate copy files
+  const destDir = path.join(__dirname, 'files-copy');
+  try {
+    if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir);
     }
-   else {
-     //read and copy content from source to destination
-     sourceFiles.forEach(file => {
-         const srcFile = path.join(srcDir, file);
-         const destFile = path.join(destDir, file);
-         const readStream = fs.createReadStream(srcFile);
-         const writeStream = fs.createWriteStream(destFile);
-         readStream.pipe(writeStream);
-     });
-     console.log(sourceFiles.length + ' files copied! All done!');
-   }
-});
+  } catch (err) {
+    console.error(err);
+  }
+  // read source files and copy to destination directory
+  fs.readdir(srcDir, (error, sourceFiles) => {
+    if (error) {
+        console.log(error);
+      }
+     else {
+       //read and copy content from source to destination
+       sourceFiles.forEach(file => {
+           const srcFile = path.join(srcDir, file);
+           const destFile = path.join(destDir, file);
+           const readStream = fs.createReadStream(srcFile);
+           const writeStream = fs.createWriteStream(destFile);
+           readStream.pipe(writeStream);
+       });
+       console.log(sourceFiles.length + ' files copied! All done!');
+     }
+  });
+}
